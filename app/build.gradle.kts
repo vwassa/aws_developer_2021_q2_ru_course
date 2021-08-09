6306c5c45ae5e741da3a69a6f3b1bef3d075b0e1
vasillii_vasilev <vasilii_vasilev@epam.com>
2021-08-09 15:16:20 +0300

    added the shadowJar plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ version = "0.0.1-SNAPSHOT"
 plugins {
     id("com.vwassa.example.kotlin-application-conventions")
     kotlin("plugin.serialization") version "1.4.32"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 dependencies {
